b18fcf45e55b58ac268f0c0edd495c6105f2a2f4
seanmin21 <seanmin@andrew.cmu.edu>
2026-01-21 20:01:11 -0500

    Update stretch_api.py

--- Lab1/stretch_api.py
+++ Lab1/stretch_api.py
@@ -9,31 +9,31 @@ robot.stow()
 robot.arm.move_to(0.5)
 robot.lift.move_to(1.0)
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 
 robot.end_of_arm.move_to('wrist_yaw', np.radians(30))
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 
 robot.end_of_arm.move_to('wrist_pitch', np.radians(30))
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 
 robot.end_of_arm.move_to('wrist_roll', np.radians(30))
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 
 robot.end_of_arm.move_to('stretch_gripper', 50)
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 
 robot.end_of_arm.move_to('stretch_gripper', 0)
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 
 robot.head.move_by('head_pan', np.radians(45)) # Move head pan
 robot.head.move_by('head_tilt', np.radians(45)) # Move head tilt
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 
 robot.stow()
