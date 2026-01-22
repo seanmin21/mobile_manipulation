5c70038e21d619eb5279946e96be11d60b9c242d
seanmin21 <seanmin@andrew.cmu.edu>
2026-01-22 00:31:37 +0000

    added some lines

--- Lab1/stretch_api.py
+++ Lab1/stretch_api.py
@@ -1,3 +1,5 @@
+import time
+import numpy as np
 import stretch_body.robot
 robot = stretch_body.robot.Robot()
 robot.startup()
@@ -7,3 +9,27 @@ robot.stow()
 robot.arm.move_to(0.5)
 robot.lift.move_to(1.0)
 robot.push_command()
+
+robot.arm.wait_until_at_setpoint()
+
+robot.end_of_arm.move_to('wrist_yaw', np.radians(30))
+robot.arm.wait_until_at_setpoint()
+
+robot.end_of_arm.move_to('wrist_pitch', np.radians(30))
+robot.arm.wait_until_at_setpoint()
+
+robot.end_of_arm.move_to('wrist_roll', np.radians(30))
+robot.arm.wait_until_at_setpoint()
+
+robot.end_of_arm.move_to('stretch_gripper', 50)
+robot.push_command()
+robot.arm.wait_until_at_setpoint()
+
+robot.end_of_arm.move_to('stretch_gripper', 0)
+robot.push_command()
+robot.arm.wait_until_at_setpoint()
+
+robot.head.move_by('head_pan', np.radians(45)) # Move head pan
+robot.head.move_by('head_tilt', np.radians(45)) # Move head tilt
+robot.push_command()
+robot.arm.wait_until_at_setpoint()
