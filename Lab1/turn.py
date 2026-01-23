65d13dff121dcd5aad278e8ba8a6ae4fdf16e236
seanmin21 <seanmin@andrew.cmu.edu>
2026-01-22 19:27:23 -0500

    Update turn.py

--- Lab1/turn.py
+++ Lab1/turn.py
@@ -1,1 +1,10 @@
+import time
+import numpy as np
+import stretch_body.robot
+robot = stretch_body.robot.Robot()
+robot.startup()
 
+robot.stow()
+
+robot.base.rotate_by(np.radians(180))
+robot.push_command()
